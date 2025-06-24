2277f5ac43cd63b544461ea9e53797434163dffc
Angel S. Moreno <angelxmoreno@gmail.com>
2025-06-23 22:15:40 -0400

    chore: personalized site - proper social links - updated meta tags - create sitemap - added GA tags

---
+++ src/lib/socialLinks.ts
@@ -0,0 +1,76 @@
+import {SiInstagram, SiTiktok, SiYoutube} from '@icons-pack/react-simple-icons';
+
+export const socialLinks = [
+    {
+        id: 'instagram',
+        name: 'Instagram',
+        handle: '@autumn_gwenniviere_',
+        url: 'https://instagram.com/autumn_gwenniviere_',
+        icon: SiInstagram,
+        color: 'from-lime-400 to-emerald-500',
+        // description: 'Behind the scenes & daily moments'
+    },
+    {
+        id: 'tiktok',
+        name: 'TikTok',
+        handle: '@gwenngwenncontent',
+        url: 'https://tiktok.com/@gwenngwenncontent',
+        icon: SiTiktok,
+        color: 'from-lime-400 to-emerald-500',
+        description: "Gwenn's Content Channel - Music, Vlogs, Etc"
+    },
+    {
+        id: 'tiktok',
+        name: 'TikTok',
+        handle: '@xxgwennivierexx',
+        url: 'https://tiktok.com/@xxgwennivierexx',
+        icon: SiTiktok,
+        color: 'from-lime-400 to-emerald-500',
+        description: 'Personal Page'
+    },
+    {
+        id: 'youtube',
+        name: 'YouTube',
+        handle: 'Autumn Gwenniviere',
+        url: 'https://www.youtube.com/@autumngwenniviere',
+        icon: SiYoutube,
+        color: 'from-lime-400 to-emerald-500',
+        description: 'Old videos soon to be updated'
+    },
+    // {
+    //     id: 'twitter',
+    //     name: 'Twitter',
+    //     handle: '@stellaperforms',
+    //     url: 'https://twitter.com/stellaperforms',
+    //     icon: Twitter,
+    //     color: 'from-green-400 to-emerald-600',
+    //     description: 'Updates & thoughts'
+    // },
+    // {
+    //     id: 'facebook',
+    //     name: 'Facebook',
+    //     handle: 'Stella Performs',
+    //     url: 'https://facebook.com/stellaperforms',
+    //     icon: Facebook,
+    //     color: 'from-lime-500 to-green-700',
+    //     description: 'Events & community'
+    // },
+    // {
+    //     id: 'spotify',
+    //     name: 'Spotify',
+    //     handle: 'Stella',
+    //     url: 'https://open.spotify.com/artist/stella',
+    //     icon: Music,
+    //     color: 'from-green-500 to-green-600',
+    //     description: 'Stream my latest tracks'
+    // },
+    // {
+    //     id: 'booking',
+    //     name: 'Book Me',
+    //     handle: 'Events & Shows',
+    //     url: 'mailto:booking@stellaperforms.com',
+    //     icon: Calendar,
+    //     color: 'from-emerald-500 to-lime-500',
+    //     description: 'Performance inquiries'
+    // }
+];
